10490760e4f559a623d8f4d5cd8cd74a4b5ca2ca
nucleaG <nuclea.g@gmail.com>
2025-12-02 11:36:52 +0100

    new fact button renderized

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
 import { newFact } from '../js/fact-button.js';
-let contentViewer = document.getElementById('viewer');
-
-
-
+let contentViewer = document.getElementById('fact');
 
 document.getElementById("new-fact-btn")?.addEventListener("click", async () => {
     let fact = await newFact();
-   /* fact.then((text) => {
-        contentViewer.textContent = text;*/
         contentViewer.innerHTML = '';
         const factViewer = document.createElement('p');
         factViewer.classList.add('fact-text');
