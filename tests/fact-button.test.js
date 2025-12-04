eb99e9f56a2e539fec11d89c90fd245a5fcad619
nucleaG <nuclea.g@gmail.com>
2025-12-04 10:55:28 +0100

    feat: new test from fact button. Fix: change mock to spy for better solutions

--- tests/fact-button.test.js
+++ tests/fact-button.test.js
@@ -1,12 +1,26 @@
 import {newFact} from '../js/fact-button.js';
-import{describe,it,expect, vi} from'vitest';
+import{describe,it,expect, vi, afterEach} from'vitest';
 
-vi.mock('../js/fact-button.js',() => ({
-    newFact: vi.fn(() => Promise.resolve('this is NOT a fact. Beee!')),
-}))
+let fetchSpy;
 describe('test to fact button',() =>{
-    it('should be take a random fact', async () =>{
+    afterEach(() => {
+        if (fetchSpy) {
+            fetchSpy.mockRestore(); 
+        }
+    });
+        it('should be take a random fact', async () =>{
+        const mockSuccessResponse = { text: 'this is NOT a fact. Beee!' };
+        fetchSpy = vi.spyOn(global, 'fetch').mockResolvedValue({
+            json: vi.fn().mockResolvedValue(mockSuccessResponse),
+        });
         const result = await newFact();
         expect(result).toBe('this is NOT a fact. Beee!');
-    });
+        expect(fetchSpy).toHaveBeenCalledTimes(1)
+        });
+        it("should to be an error message if the API don't works", async() =>{
+        fetchSpy = vi.spyOn(global, 'fetch').mockRejectedValue(new Error('Simulated network failure'));
+        const result = await newFact();
+        expect(result).toBe('Could not fetch a new fact at this time.');
+       
+        });
 });
